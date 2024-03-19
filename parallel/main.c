5cbaf72cf67d8c4943349647587c0add9eac7add
jpxx-arts <jpramalho123@hotmail.com>
2024-03-19 14:39:53 -0300

    Adding current time_spent omp

--- parallel/main.c
+++ parallel/main.c
@@ -1,39 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-#include <time.h>
 #include "alg_lin.h"
 
-#define ROWS 5000
-#define COLUMNS 5000
+#define ROWS 2000
+#define COLUMNS 2000
 
-int main() {
-    clock_t begin = clock();
+int main(int argc, char const *argv[]) {
+    double start = omp_get_wtime();
 
     int **m1 = NULL;
     create_matrix(&m1, ROWS, COLUMNS);
     auto_fill(&m1, ROWS, COLUMNS, 1);
-    printf("matrix m1:\n");
-    show_matrix(m1, ROWS, COLUMNS);
 
     int **m2 = NULL;
     create_matrix(&m2, ROWS, COLUMNS);
     auto_fill(&m2, ROWS, COLUMNS, 2);
-    printf("matrix m2:\n");
-    show_matrix(m2, ROWS, COLUMNS);
 
     int **res = NULL;
     create_matrix(&res, ROWS, COLUMNS);
     multiply_matrices(m1, m2, res, ROWS, COLUMNS, ROWS, COLUMNS);
-    printf("Matrix multiplication between m1 and m2:\n");
-    show_matrix(res, ROWS, COLUMNS);
 
     free_matrix(m1, ROWS);
     free_matrix(m2, ROWS);
     free_matrix(res, ROWS);
 
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    double end = omp_get_wtime();
+    double time_spent = (end - start);
     printf("%f s\n", time_spent);
 
     return 0;
